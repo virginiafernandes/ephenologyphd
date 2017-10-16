de938d4966a3893816974bf507bbf841d07cd2ae
Virgínia Mota <vivifernandesmota@MacBook-Pro-de-Virginia-Mota.local>
2017-10-16 15:40:02 -0200

    with mask position

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
 import sys
 import cv2
+import numpy as np
 
 def read_imagelist(fname, imagelist):
 	with open(fname, 'r') as f:
@@ -8,7 +9,20 @@ def read_imagelist(fname, imagelist):
 			imagelist.append(line) 
 	return;
 
-def extracting_mask(new_img, maskimg, image):
+def extracting_mask(new_img, mask_img, image):
+	x = 0
+	i = 0
+	j = 0
+	[a, b, c] = mask_img.shape
+	mask_position =[]
+	for i in range(0, a):
+		for j in range(0,b):
+			color = mask_img[i,j,:]
+			if color[0] == 255:
+				mask_position.append([i,j])
+				x += 1
+	#new_img = cv2.resize(new_img, (x, y))
+	#new_img = imwrite('testing.jpg', new_img)
 	return;
 
 
@@ -32,6 +46,7 @@ img1 = cv2.imread(imagelist[0][0:16])
 #cv2.imshow('image', img1)
 
 #separate mask + image
+new_img = cv2.imread('testing.jpg')
 extracting_mask(new_img, maskimg, img1)
 
 
