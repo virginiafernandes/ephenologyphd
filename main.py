4c96002a7abb0c2732ca4177e1e5506011174f8c
Virgínia Mota <vivifernandesmota@MacBook-Pro-de-Virginia-Mota.local>
2017-10-16 11:59:36 -0200

    trying mask

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import sys
+import cv2
 
 #read mask
 #read image
 
+mask = str(sys.argv[1])
+image = str(sys.argv[2])
 
+print 'Working on mask', mask
+print 'Working on observation', image
+
+maskimg = cv2.imread(mask)
+cv2.imshow('image', maskimg)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 #separate mask + image
 
