49bb280fb662096e3bc07a33890702aca951cf7b
Virgínia Mota <vivifernandesmota@MacBook-Pro-de-Virginia-Mota.local>
2017-10-16 12:13:44 -0200

    README.md

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 import cv2
 
+def read_imagelist(str):
+	print str 
+	return;
+
 #read mask
-#read image
+#read imagelist
 
 mask = str(sys.argv[1])
 imagelist = str(sys.argv[2])
@@ -13,6 +17,7 @@ print 'Working on observations', imagelist
 maskimg = cv2.imread(mask)
 #cv2.imshow('image', maskimg)
 
+read_imagelist(imagelist)
 
 #separate mask + image
 
